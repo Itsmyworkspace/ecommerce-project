420be18e22f91a1c870fc5f0ba4371cf76764ea4
Vineethkumar <itsmevinubujji143@gmail.com>
2026-07-26 02:33:04 +0530

    ecommerce code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import "./styles/App.css";
+import { Routes, Route } from "react-router";
+import Homepage from "./components/HomePage";
+import CheckOut from "./components/CheckOut";
+import OrderPage from "./components/OrderPage";
+import TrackingPage from "./components/TrackingPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route index element={<Homepage />}></Route>
+      <Route path="checkout" element={<CheckOut />}></Route>
+      <Route path="orders" element={<OrderPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
